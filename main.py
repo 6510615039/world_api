5204142ce04ab7d9523308ef3090de90d28a240f
Kwan Daorueang <6510615039@cn330>
2024-11-05 20:52:06 +0700

    Refactor code to read data from csv

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import csv
 from fastapi import FastAPI
+from utils import get_data_from_csv
 
-# get country data from csv file  
-filename = "world_table_country.csv"
-with open(filename, "r", encoding="utf-8") as csv_file:
-    csv_reader = csv.reader(csv_file)
-    headers = next(csv_reader)
-    data_country = [{k: v for (k, v) in zip(headers, row)} for row in csv_reader]
+data_country = get_data_from_csv("world_table_country.csv")
+data_city = get_data_from_csv("world_table_city.csv")
 
-# create an instance of class FastAPI named "app"
 app = FastAPI()
 
-# define function that handles "GET" request with endpoint "/"
 @app.get("/")
 async def read_root() -> dict:
     return {"message": "Hello there"}
 
-# define function that handles "GET" request with endpoint "/world"
 @app.get("/world")
 async def read_countries() -> dict:
     return {"result": data_country}
+
+@app.get("/world/country/{name}")
+async def read_country(name: str) -> dict:
+    for row in data_country:
+        if row["Name"].lower() == name.lower():
+            return {"result": row}
+    return {"result": {}}
+
+@app.get("/world/city/{name}")
+async def read_city(name: str) -> dict:
+    for row in data_city:
+        if row["Name"].lower() == name.lower():
+            return {"result": row}
+    return {"result": {}}
